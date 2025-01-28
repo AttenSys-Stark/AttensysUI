5dc29c449987952197761a5b33874c7132ee741c
Collins C Augustine <colins4chinonso@gmail.com>
2025-01-28 23:38:51 +0100

    feat: (#45) Implement responsive features/styling for sponsorship pages

--- src/components/overview/Sponsorship.tsx
+++ src/components/overview/Sponsorship.tsx
@@ -1,59 +1,81 @@
 import React from 'react'
 import Sponsorlist from './Sponsorlist'
-import {approvedsponsors,pendingsponsors} from '@/constants/data'
+import {approvedsponsors, pendingsponsors} from '@/constants/data'
 
 const Sponsorship = () => {
   return (
     <div className="h-auto w-full pb-10">
-        <h1 className='mb-5 w-[80%] mx-auto font-semibold text-[18px] text-[#333333]'>Sponsorship</h1>
-        <div className='h-[360px] rounded-lg w-[80%] mx-auto bg-[#FFFFFF] border-[1px] border-[#DEDEDE] overflow-y-auto '>
-        <table className="w-full border-spacing-y-3 ">
-                <thead>
-                <tr className="h-[56px] text-[14px] border-b-[2px] font-normal leading-[19.79px]">
-                    <th className=" text-center font-light border-r-[2px] border-[#E9EBEC]">Sponsors</th>
-                    <th className=" text-center font-light border-r-[2px] border-[#E9EBEC]">Amount</th>
-                    <th className=" text-center font-light border-r-[2px] border-[#E9EBEC]">Wallet address</th>
-                    <th className=" text-center font-light border-r-[2px] border-[#E9EBEC]">Time stamp</th>
-                    <th className="text-center font-light">Contact</th>
-                </tr>
-                </thead>
-                <tbody>
-                    {approvedsponsors.map((data, index) => (
-                    <Sponsorlist 
-                        key={index} 
-                        name={data.name} 
-                        icon={data.icon} 
-                        address={data.address} 
-                        amount={data.amount} 
-                        time={data.time} 
-                    />
-                    ))}
-                </tbody>
-            </table>
+      <h1 className='mb-5 w-[90%] md:w-[80%] mx-auto font-semibold text-base md:text-[18px] text-[#333333]'>
+        Sponsorship
+      </h1>
+      <div className='min-h-[360px] rounded-lg w-[90%] md:w-[80%] mx-auto bg-[#FFFFFF] border-[1px] border-[#DEDEDE] overflow-x-auto'>
+        <div className='min-w-[800px] md:min-w-0'> 
+          <table className="w-full border-spacing-y-3">
+            <thead>
+              <tr className="h-[56px] text-xs md:text-[14px] border-b-[2px] font-normal leading-[19.79px]">
+                <th className="text-center font-light border-r-[2px] border-[#E9EBEC] px-2 md:px-4">
+                  Sponsors
+                </th>
+                <th className="text-center font-light border-r-[2px] border-[#E9EBEC] px-2 md:px-4">
+                  Amount
+                </th>
+                <th className="text-center font-light border-r-[2px] border-[#E9EBEC] px-2 md:px-4">
+                  Wallet address
+                </th>
+                <th className="text-center font-light border-r-[2px] border-[#E9EBEC] px-2 md:px-4">
+                  Time stamp
+                </th>
+                <th className="text-center font-light px-2 md:px-4">
+                  Contact
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {approvedsponsors.map((data, index) => (
+                <Sponsorlist 
+                  key={index} 
+                  name={data.name} 
+                  icon={data.icon} 
+                  address={data.address} 
+                  amount={data.amount} 
+                  time={data.time} 
+                />
+              ))}
+            </tbody>
+          </table>
 
-            <table className="w-full border-spacing-y-3 border-b-[2px]">
-                <thead>
-                <tr className="h-[56px] text-[14px] border-y-[2px] font-normal leading-[19.79px]">
-                    <th className=" text-center font-light text-[#DC1D16]">Pending</th>
-                    <th className=" text-center font-light text-[#DC1D16]">Amount</th>
-                    <th className=" text-center font-light text-[#DC1D16]">Wallet address</th>
-                    <th className=" text-center font-light text-[#DC1D16]">Time stamp</th>
-                </tr>
-                </thead>
-                <tbody>
-                    {pendingsponsors.map((data, index) => (
-                    <Sponsorlist 
-                        key={index} 
-                        name={data.name} 
-                        icon={data.icon} 
-                        address={data.address} 
-                        amount={data.amount} 
-                        time={data.time} 
-                    />
-                    ))}
-                </tbody>
-            </table>
+          <table className="w-full border-spacing-y-3 border-b-[2px]">
+            <thead>
+              <tr className="h-[56px] text-xs md:text-[14px] border-y-[2px] font-normal leading-[19.79px]">
+                <th className="text-center font-light text-[#DC1D16] px-2 md:px-4">
+                  Pending
+                </th>
+                <th className="text-center font-light text-[#DC1D16] px-2 md:px-4">
+                  Amount
+                </th>
+                <th className="text-center font-light text-[#DC1D16] px-2 md:px-4">
+                  Wallet address
+                </th>
+                <th className="text-center font-light text-[#DC1D16] px-2 md:px-4">
+                  Time stamp
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {pendingsponsors.map((data, index) => (
+                <Sponsorlist 
+                  key={index} 
+                  name={data.name} 
+                  icon={data.icon} 
+                  address={data.address} 
+                  amount={data.amount} 
+                  time={data.time} 
+                />
+              ))}
+            </tbody>
+          </table>
         </div>
+      </div>
     </div>
   )
 }
